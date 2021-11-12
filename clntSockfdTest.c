71d1d0fc5aa105fd1a8e7833f393d83ee18e59fb
3351-dev <3351.dev@gmail.com>
2021-11-12 18:10:07 +0900

    1112 subject

---
+++ clntSockfdTest.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void print_array(int *arr, int argc);
+
+
+int main(int argc, char **argv)
+{
+	int fds[256] = {-1, };
+
+	// fds init
+	for(int i=0;i<256;i++){
+		fds[i] = -1;
+	}
+
+	for(int i=0;i<argc-1;i++){
+		fds[i] = atoi(argv[i+1]);
+	}
+
+	print_array(fds, argc);
+
+	for(int i=0;i<argc-1;i++){
+		for(int j=i+1;j<argc;j++){
+			if(fds[i]==fds[j]){
+				fds[i] = -1;
+				fds[j] = -1;
+			}
+		}
+		if(fds[i] == -1){
+			for(int j=i;j<sizeof(fds)/sizeof(int)-1;j++){
+				fds[j] = fds[j+1];
+			}
+		}
+	
+
+	print_array(fds,argc);
+
+//	printf("sizeof(fds) : %ld\n", sizeof(fds)/sizeof(int));
+	}
+}
+
+void print_array(int *arr, int argc)
+{
+	for(int i=0;i<argc;i++){
+		printf("%d ",arr[i]);
+		if(arr[i]==-1){
+			break;
+		}
+	}
+	printf("\n");
+}
