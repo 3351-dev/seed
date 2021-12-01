7e941b3a2287d0adaa2f97ffeb72cb36f324bc54
3351-dev <3351.dev@gmail.com>
2021-12-01 18:04:18 +0900

    1201 update

--- 1130/fr.c
+++ 1130/fr.c
@@ -8,10 +8,21 @@
 
 #include "friend.h"
 
+#define LIST_NUM 5
 #define MAX 1024
 #define DATA "1109.dat"
+#define HOME (((opt-1)/LIST_NUM)+1)
+
+char header[] ={ 
+			"<!DOCTYPE html> <html> <head> <title> Fr </title> </head>"
+			"<body> <h1 align=\"center\"> Friends List &nbsp </h1>"
+};
+char end[]={
+			"</body></html>"
+};
 
 int friends(int select, char *option);
+int friends_rdwr(int select, char *option);
 
 int main(int argc, char **argv)
 {
@@ -23,16 +34,35 @@ int main(int argc, char **argv)
 	if(strstr(local_url, "list")){
 		option = strtok(local_url,"=");
 		option = strtok(NULL," ");
+		if(atoi(option)<=0){
+			friends(1,"1");
+			return -1;
+		}
 		friends(1,option);
 	}else if(strstr(local_url, "detail")){
 		option = strtok(local_url,"=");
 		option = strtok(NULL," ");
+		if(atoi(option)<=0){
+			friends(2,"1");
+			return -1;
+		}
 		friends(2,option);
 	}else if(strstr(local_url, "search")){
 		option = strtok(local_url,"=");
 		option = strtok(NULL," ");
+		if(option==NULL){
+			friends(1, "1");
+			return -1;
+		}
 		friends(3,option);
+	}else if(strstr(local_url, "addValue")){
+		option = strtok(local_url,"=");
+		option = strtok(NULL, " ");
+		friends_rdwr(1, option);
+	}else if(strstr(local_url, "name")){
+		friends_rdwr(2,local_url);
 	}else{
+		printf("Error... reset page...<br>");
 		friends(1,"1");
 	}
 	return 0;
@@ -51,11 +81,14 @@ int friends(int select, char *option)
 
 	opt = atoi(option);
 
-	sprintf(Buf,"<!DOCTYPE html> <html> <head> <title> Fr </title> </head>"
-			"<body> <h1> Friends List </h1>"
-			"<form action\"./fr.cgi\" method=\"get\">"
+	sprintf(Buf,
+			"%s"
+			"<div align=\"center\">"
+			"<form action=\"./fr.cgi\" method=\"get\">"
 			"<input type=\"search\" name=\"search\">"
-			"<input type=\"submit\"> </form><br>"
+			"<input type=\"submit\" value=\"Search\"> </form> </div>"
+			"<h5 align=\"center\"> #%d &nbsp </h5>"
+			, header,opt
 		   );
 
 	printf("%s\n", Buf);
@@ -63,29 +96,44 @@ int friends(int select, char *option)
 
 	// 1 Show Friends List
 	if(select == 1){
-//		char arr[MAX], ages[10];
-		for(int i=15*(opt-1); i<15*opt; i++){
+		sprintf(Buf,
+			"<div align=\"center\">"
+			"<a href=\"./fr.cgi?list=%d\"><button> Pre </button></a>&nbsp"
+			"<a href=\"./fr.cgi?list=%d\"><button> &#9829 </button></a>&nbsp"
+			"<a href=\"./fr.cgi?list=%d\"><button> Next</button></a>"
+			"</div>"
+			, opt-1, opt ,opt+1
+			);
+		printf("%s\n", Buf);
+		for(int i=LIST_NUM*(opt-1); i<LIST_NUM*opt; i++){
 			lseek(frifd, i*sizeof(fr), SEEK_SET);
 			if(read(frifd, &fr, sizeof(fr))<=0){
-				sprintf(Buf,"END");
-				printf("%s\n",Buf);
 				break;
-			}else{
-				sprintf(content,"%s / %d / %s / %s / %s\n", fr.name, fr.age, fr.phone, fr.email, fr.address);
+			}
+			else{
+				sprintf(content,"%s / %s\n", fr.name, fr.phone);
 				sprintf(Buf, "<p>[%03d] <a href=\"./fr.cgi?detail=%d\"> %s </a> </p>", i+1, i+1, content);
 				printf("%s\n", Buf);
 			}
 		}
 	}
+
 	// 2 Show Detail
 	else if(select == 2){
 		lseek(frifd, (opt-1)*sizeof(fr), SEEK_SET);
 		read(frifd, &fr, sizeof(fr));
-
-		sprintf(Buf, " Name: %s<br>Age: %d<br>Phone: %s<br>E-mail: %s<br>Address: %s<br>Favorite: %d<br>"
-				, fr.name, fr.age, fr.phone, fr.email,fr.address, fr.flag);
+		sprintf(Buf,
+				"<div align=\"center\">"
+				"<a href=\"./fr.cgi?detail=%d\"><button> Pre </button></a>&nbsp"
+				"<a href=\"./fr.cgi?list=%d\"><button> &#9829 </button></a>&nbsp"
+				"<a href=\"./fr.cgi?detail=%d\"><button> Next</button></a>"
+				"</div>"
+				, opt-1, HOME,opt+1
+			   );
 		printf("%s\n", Buf);
-		sprintf(Buf, "<p> <a href=\"./fr.cgi?list=%d\"> Home </a></p>", 1);
+
+		sprintf(Buf, "<h2>Name: %s</h2>Age: %d<br>Phone: %s<br>E-mail: %s<br>Address: %s<br>Favorite: %d<br>"
+				, fr.name, fr.age, fr.phone, fr.email,fr.address, fr.flag);
 		printf("%s\n", Buf);
 	}
 
@@ -105,10 +153,98 @@ int friends(int select, char *option)
 		}
 	}
 
-	close(frifd);
 
-	sprintf(Buf, "<p> select : %d </p> </body></html>",select);
+	int endNum;
+	endNum = lseek(frifd, 0, SEEK_END)/sizeof(fr);
+
+//	printf("opt : %d<br>end : %d<br>",opt,endNum);
+
+
+	sprintf(Buf, 
+			"<a href=\"./fr.cgi?addValue%d\">"
+			"<button> add </button> <br>"
+			"</a>"
+			,endNum+1
+			);
 	printf("%s\n", Buf);
+	printf("%s\n", end);
 
+	close(frifd);
 	return 0;
 }
+
+int friends_rdwr(int select, char *option)
+{
+	int frifd, th, opt;
+	char buf[MAX], *ptrOpt, *ptrValue;
+	char *menu[5]={"name", "age", "phone", "email", "address"};
+	FRIEND_T fr;
+
+	if((frifd=open(DATA, O_WRONLY, 0755))==-1){
+		printf("open failed\n");
+		return -1;
+	}
+
+	sprintf(buf,"%s",header);
+	printf("%s", buf);
+	
+	
+	// add form 
+	if(select==1){
+		opt = atoi(option);
+		th = lseek(frifd, 0, SEEK_END)/sizeof(fr)+1;
+
+		sprintf(buf,
+//				"%s"
+				"<h5 align=\"center\"> #%d &nbsp </h5>"
+				"<form action=\"./fr.cgi\" method=\"get\">"
+				,opt
+			   );
+		printf("%s", buf);
+
+		printf("th : %d<br>", th);
+		for(int i=0;i<5;i++){
+			sprintf(buf,
+					"%s<br>"
+					"<input type=\"text\" name=\"%s\"><br>",menu[i],menu[i]);
+			printf("%s\n",buf);
+		}
+		
+		sprintf(buf, "<input type=\"submit\" value=\"submit\"> </form>");
+		printf("%s", buf);
+
+	// add input
+	}else if(select == 2){
+//		ptrOpt = option;
+		for(int i=0;i<5;i++){
+			ptrOpt = option;
+
+			for(int j=0;j<i;j++){
+				ptrOpt=strtok(ptrOpt,"&");
+				ptrOpt=strtok(NULL,"&");
+				ptrOpt=strtok(NULL,"&");
+			}
+
+			printf("ptrOpt : %s<br>",ptrOpt);
+			ptrOpt = strtok(ptrOpt,"&");
+			ptrValue = strtok(ptrOpt,"=");
+			ptrValue = strtok(NULL,"=");
+			printf("&nbsp ptrOpt : %s<br>&nbsp ptrValue : %s<br>", ptrOpt,ptrValue);
+/*
+			ptrOpt=option;
+			ptrOpt = strtok(ptrOpt, "&");
+			ptrOpt = strtok(NULL, "=");
+*/
+
+			printf("<br>");
+		}
+
+	}
+
+
+
+	printf("%s\n", end);
+	close(frifd);
+	return 0;
+}
+
--- 1130/readme.md
+++ 1130/readme.md
@@ -1,6 +1,6 @@
 @1130
 
-[Err]
+Err
 ~~~using 127.0.1.1 set the 'ServerName' directive globally to suppress this message
 >> vi /etc/apache2/apache2.conf
 >> ServerName "~~~"
@@ -8,19 +8,23 @@
 >> sudo service apache2 restart
 
 
-[Info]
+Info
 Apache2를 재시작 할때는 다음의 코드를 사용하자
 어디서 오류가 났는지 알려준다.
 >> sudo apache2ctl restart
 
-[Info]
+Info
 apache2 에러로그 위치
 >> /var/log/apache2
 
-[MEMO]
+Memo
 Content-type: text/html
 Content-type : text/html X
 
-
+@1201
+hosts
+/etc/hosts
+ localhost >> myhost.com
+ 
 
 
