1df662e358e770b6353dce52eb461a54f91d7f8f
3351-dev <3351.dev@gmail.com>
2022-01-18 18:06:19 +0900

    0118

--- Android/BookRecyclerBiewExam2/app/src/main/java/com/example/bookrecyclerbiewexam2/Adapter.java
+++ Android/BookRecyclerBiewExam2/app/src/main/java/com/example/bookrecyclerbiewexam2/Adapter.java
@@ -4,6 +4,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -15,14 +17,27 @@ import java.util.List;
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
     private final String TAG = this.getClass().getSimpleName();
     private final List<CardItem> mDataList;
+    private AdapterClickListener mListener;
+
+    public void setOnClickListener(AdapterClickListener listener){
+        mListener = listener;
+    }
+    public interface AdapterClickListener{
+        void onItemClicked(int position);
+        void onBtn1Clicked(int position);
+        void onBtn2Clicked(int position);
+    }
+
     public Adapter(List<CardItem> dataList){
         mDataList = dataList;
     }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_card, parent, false);
+
         return new ViewHolder(view);
     }
 
@@ -30,7 +45,36 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         CardItem item = mDataList.get(position);
         holder.title.setText(item.getTitle());
-        holder.contents.setText(item.getContents());
+        holder. contents.setText(item.getContents());
+
+        if(mListener != null){
+            final int pos = position;
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d(TAG, pos+" click");
+                    mListener.onItemClicked(pos);
+
+                }
+            });
+            holder.btn1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d(TAG, pos+" click");
+                    mListener.onBtn1Clicked(pos);
+
+                }
+            });
+            holder.btn2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d(TAG, pos+" click");
+                    mListener.onBtn2Clicked(pos);
+
+                }
+            });
+
+        }
     }
 
     @Override
@@ -40,10 +84,13 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
         private final String TAG = this.getClass().getSimpleName();
+
         TextView title;
         TextView contents;
-        TextView fold;
+        ImageButton fold;
         LinearLayout contentsPart;
+        Button btn1;
+        Button btn2;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -51,6 +98,8 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
             contents = itemView.findViewById(R.id.contents_text);
             fold = itemView.findViewById(R.id.contents_fold);
             contentsPart = itemView.findViewById(R.id.contents_part);
+            btn1 = itemView.findViewById(R.id.btn_1);
+            btn2 = itemView.findViewById(R.id.btn_2);
 
             title.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -65,9 +114,23 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder>{
                 public void onClick(View view) {
                     Log.d(TAG, getAdapterPosition()+" fold");
                     contentsPart.setVisibility(View.GONE);
-                    contentsPart.src
+                    fold.setImageResource(R.drawable.ic_baseline_keyboard_arrow_down_24);
                 }
             });
+
         }
+
+    }
+
+    public void removeItem(int position){
+        mDataList.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, mDataList.size());
+    }
+
+    public void addItem(int position, CardItem item){
+        mDataList.add(position, item);
+        notifyItemInserted(position);
+        notifyItemRangeChanged(position, mDataList.size());
     }
 }
--- Android/BookRecyclerBiewExam2/app/src/main/java/com/example/bookrecyclerbiewexam2/MainActivity.java
+++ Android/BookRecyclerBiewExam2/app/src/main/java/com/example/bookrecyclerbiewexam2/MainActivity.java
@@ -5,11 +5,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Adapter.AdapterClickListener{
+
+    private final String TAG = this.getClass().getSimpleName();
+    private Adapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +29,28 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0;i<5;i++){
             dataList.add(new CardItem(""+i+"'s Item",""+i+"'s Contents"));
         }
-        Adapter adapter = new Adapter(dataList);
-        recyclerView.setAdapter(adapter);
+
+        // Adapter Setting
+        mAdapter = new Adapter(dataList);
+            // ClickListener
+        mAdapter.setOnClickListener(this);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onItemClicked(int position) {
+//        Log.d(TAG, position+" item clicked");
+    }
+
+    @Override
+    public void onBtn1Clicked(int position) {
+//        Log.d(TAG, position+" Btn 1 clicked");
+        mAdapter.addItem(position, new CardItem("Add","Add"));
+    }
+
+    @Override
+    public void onBtn2Clicked(int position) {
+//        Log.d(TAG, position+ " Btn 2 clicked");
+        mAdapter.removeItem(position);
     }
 }
