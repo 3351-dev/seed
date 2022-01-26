7d1e04931982a6ec0d7172f07d945dbaeb592ea1
3351-dev <3351.dev@gmail.com>
2022-01-26 09:06:33 +0900

    0126

--- NLP/eng_word2Vec.py
+++ NLP/eng_word2Vec.py
@@ -6,22 +6,27 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 
 #urllib.request.urlretrieve("https://raw.githubusercontent.com/ukairia777/tensorflow-nlp-tutorial/main/09.%20Word%20Embedding/dataset/ted_en-20160408.xml", filename="ted_en-20160408.xml")
 
-targetXML = open('ted_en-20160408.xml', 'r', encoding='UTF8')
-target_text = etree.parse(targetXML)
+# targetXML = open('ted_en-20160408.xml', 'r', encoding='UTF8')
+# target_text = etree.parse(targetXML)
 
-# xml 파일로부터 <content>와 </content> 사이의 내용만 가져온다.
-parse_text = '\n'.join(target_text.xpath('//content/text()'))
+# # xml 파일로부터 <content>와 </content> 사이의 내용만 가져온다.
+# parse_text = '\n'.join(target_text.xpath('//content/text()'))
 
-# 정규 표현식의 sub 모듈을 통해 content 중간에 등장하는 (Audio), (Laughter) 등의 배경음 부분을 제거.
-# 해당 코드는 괄호로 구성된 내용을 제거.
-content_text = re.sub(r'\([^)]*\)', '', parse_text)
+# # 정규 표현식의 sub 모듈을 통해 content 중간에 등장하는 (Audio), (Laughter) 등의 배경음 부분을 제거.
+# # 해당 코드는 괄호로 구성된 내용을 제거.
+# content_text = re.sub(r'\([^)]*\)', '', parse_text)
 
-f = open('content_text.text','w')
-f.write(content_text)
-f.close()
+# f = open('content_text.text','w')
+# f.write(content_text)
+# f.close()
+
+
+
+f = open('content_text.text','r')
+
 
 # 입력 코퍼스에 대해서 NLTK를 이용하여 문장 토큰화를 수행.
-sent_text = sent_tokenize(content_text)
+sent_text = sent_tokenize(f.read())
 
 # 각 문장에 대해서 구두점을 제거하고, 대문자를 소문자로 변환.
 normalized_text = []
@@ -37,7 +42,7 @@ print('총 샘플의 개수 : {}'.format(len(result)))
 from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
 
-model = Word2Vec(sentences=result, size=100, window=5, min_count=5, workers=4, sg=0)
+model = Word2Vec(sentences=result, vector_size=100, window=5, min_count=5, workers=4, sg=0)
 
 model_result = model.wv.most_similar("man")
 print(model_result)
