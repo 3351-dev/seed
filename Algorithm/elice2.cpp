c8821b09ef9aa0cd784cf5a5826a1c28f9d137c3
3351-dev <3351.dev@gmail.com>
2022-04-03 00:01:26 +0900

    0403

--- Algorithm/elice2.cpp
+++ Algorithm/elice2.cpp
@@ -2,36 +2,42 @@
 
 #include<iostream>
 using namespace std;
-int gcd(int a,int b){
+// 최대 공약수
+int getGcd(int a,int b){
     int n;
     while(b!=0){
         n=a%b;
-        if(n==0) return b;
         a=b;
         b=n;
     }
     return a;
 }
+// 최소 공배수
+int getLcm(int a,int b){
+    return a*b/getGcd(a,b);
+}
 
 int main(){
-    int N, C, cnt=0, mixmatch;
+    int N, C, cnt=0;
+    int gcd,lcm;
     cin >> N >> C;
     int arr[N];
     
     for(int i=0;i<N;i++){
         cin >> arr[i];
     }
+    gcd = lcm = 1;
 
-    mixmatch = gcd(arr[0],arr[1]);
-    cout << mixmatch <<"\n";
+    // 최대 공약수 최소 공배수
+    for(int i=0;i<N;i++){
+        gcd = getGcd(gcd,arr[i]);
+        lcm = getLcm(lcm, arr[i]);
+    }
 
     for(int i=0;i<N;i++){
         cnt += C/arr[i];
     }
+    cnt -= C/lcm;
 
     cout << cnt <<"\n";
-
-
-
-
 }
