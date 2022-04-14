ca072bb22de129a4e8d3441e900f08233c9c895d
3351-dev <3351.dev@gmail.com>
2022-04-14 23:38:19 +0900

    0414

---
+++ Algorithm/1427.cpp
@@ -0,0 +1,12 @@
+// 1427.cpp
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    string n;
+    cin >> n;
+    sort(n.begin(), n.end(), greater<int>());
+    cout << n << "\n";
+}
