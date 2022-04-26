4c5e2db1b4446a23b1432c2d3934362519a785fa
3351-dev <3351.dev@gmail.com>
2022-04-26 22:52:03 +0900

    0426

---
+++ Algorithm/2579.cpp
@@ -0,0 +1,30 @@
+// 2579.cpp
+
+#include<iostream>
+#include<algorithm>
+#define MAX 301
+
+using namespace std;
+
+int dp[MAX];
+int arr[MAX];
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i=1;i<n+1;i++){
+        cin >> arr[i];
+    }
+
+    dp[1] = arr[1];
+    dp[2] = arr[1] + arr[2];
+    dp[3] = max(arr[1]+arr[3], arr[2]+arr[3]);
+
+    for(int i=4;i<n+1;i++){
+        dp[i] = max(dp[i-2] + arr[i], dp[i-3]+arr[i-1]+arr[i]);
+    }
+
+    cout << dp[n] << "\n";
+
+
+}
