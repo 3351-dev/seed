c14b00b09bef7c58e3d00af654034626a4969236
3351-dev <3351.dev@gmail.com>
2022-05-11 20:02:48 +0900

    0511

---
+++ Algorithm/15651.cpp
@@ -0,0 +1,30 @@
+// 15651.cpp
+
+#include <iostream>
+#define MAX 7
+using namespace std;
+
+int n,m;
+int arr[MAX] = {0,};
+bool visited[MAX] = {0,};
+
+void dfs(int cnt){
+    if(cnt == m){
+        for(int i=0;i<m;i++){
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    for(int i=1;i<n+1;i++){
+        visited[i] = true;
+        arr[cnt] = i;
+        dfs(cnt+1);
+        visited[i] = false;
+    }
+}
+
+int main(){
+    cin >> n >> m;
+    dfs(0);
+}
