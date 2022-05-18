a1f4a7563db67cf67e368155bc7656ace8615efe
3351-dev <3351.dev@gmail.com>
2022-05-18 22:48:36 +0900

    0518

---
+++ Algorithm/9012.cpp
@@ -0,0 +1,47 @@
+// 9012.cpp
+
+#include<iostream>
+#include<stack>
+#include<string>
+ 
+using namespace std;
+ 
+bool Check(string str){
+    int len = (int)str.length();
+    stack<char> st;
+ 
+    for(int i=0; i<len ; i++) {
+        char c = str[i];
+ 
+        if(c == '('){
+            st.push(str[i]);
+        }else{
+            if(!st.empty()){
+                st.pop();
+            }else{
+                return false;
+            }
+        }
+    }
+ 
+    return st.empty();
+}
+ 
+int main(void){
+ 
+    int n;
+    cin >> n;
+ 
+    for(int i=0; i<n; i++){
+        string str;
+        cin >> str;
+ 
+        if(Check(str)){
+            cout << "YES" << "\n";
+        }else{
+            cout << "NO" << "\n";
+        }
+    }
+ 
+    return 0;
+}
