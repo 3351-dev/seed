cbc1862c4470bcbb86f305a90a31b622dbd5e690
3351-dev <3351.dev@gmail.com>
2022-06-04 23:15:03 +0900

    0604

---
+++ Algorithm/2477.cpp
@@ -0,0 +1,34 @@
+// 2477.cpp
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int k;
+pair<int, int> line[6];
+int width=1;
+int result=0;
+
+void Input(){
+    cin>>k;
+    for(int i=0; i<6; i++){
+        cin>>line[i].first>>line[i].second;
+    }
+}
+
+int Solution(){
+    for (int i=0; i<6; i++){
+        int cur=line[i].second*line[(i+1)%6].second;
+        if(width==1||cur>width){
+            width=cur;
+            result=(width-line[(i+3)%6].second*line[(i+4)%6].second)*k;
+        }
+    }
+    return result;
+}
+
+int main(){
+    Input();
+    cout<<Solution()<<endl;
+    return 0;
+}
