fc7ad61a55fb8929dc6a6fd3a1369eeb7a5dc0ad
3351-dev <93642972+3351-dev@users.noreply.github.com>
2022-06-22 16:51:03 +0900

    Add files via upload

--- sh/scenario_converter.py
+++ sh/scenario_converter.py
@@ -2,10 +2,10 @@
 
 import re
 
-with open('0622_scenario','r',encoding='utf-8') as f:
+with open('0622_scenario.txt','r',encoding='utf-8') as f:
 	lines = f.readlines()
 
-with open('0622_scenario_2','w',encoding='utf-8') as ff:
+with open('0622_scenario_2.txt','w',encoding='utf-8') as ff:
 	for line in lines:
 		line = line.replace('(1만원권)','')
 		line = re.sub(r'\([^)]*\)', '',line)	# () 제거
@@ -32,7 +32,7 @@ with open('0622_scenario_2','w',encoding='utf-8') as ff:
 		ff.write(line)
 
 with open('0622_scenario_2','r',encoding='utf-8') as f:
-	with open('0622_scenario_3.csv','w',encoding='utf-8') as ff:
+	with open('korean_corpus.csv','w',encoding='utf-8') as ff:
 		lines = f.readlines()
 		for line in lines:
 			line = line.strip('-) ')
@@ -45,3 +45,7 @@ with open('0622_scenario_2','r',encoding='utf-8') as f:
 				line += str(cnt)+'\n'
 				ff.write(line)
 
+# 산출된 파일의 이름을 korean_corpus.csv 혹은 english_corpus.csv로 변경하여 mimic-recording-studio의 backend폴더에 넣으십시오.
+# 0622_scenario 파일을 읽어 0622_scenario_2로 만듭니다. 이를가지고 korea_corpus.csv 파일을 만듭니다.
+# 대본 형식은 일관되지 않으므로 상황에 맞게 편집해주세요.
+# 제 경우 string으로 만든 후 알맞게 잘라주는 방식을 택했습니다.
